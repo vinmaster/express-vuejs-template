ba197c921d871d789571fe5ab46d1134c1f76e35
Vincent Ho <vinmaster99@gmail.com>
2020-10-04 23:31:00 -0700

    Remove previous vue setup

--- client/src/routes.js
+++
@@ -1,7 +0,0 @@
-import Home from './components/Home.vue';
-import Login from './components/Login.vue';
-
-export const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-];
