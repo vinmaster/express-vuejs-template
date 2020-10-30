930abaab7cccf4fa7860830b03c400911a728f28
Vincent Ho <vinmaster99@gmail.com>
2020-10-29 23:36:46 -0700

    REMOVE

--- server/routes.ts
+++ server/routes.ts
@@ -6,7 +6,8 @@ const routes = express.Router();
 routes.use('/api', apiRoutes);
 
 // Send to client spa
-routes.get('*', function (req, res) {
+routes.get('*', function(req, res) {
+  console.log('ip', req.ip, req.headers['x-forwarded-for']);
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
